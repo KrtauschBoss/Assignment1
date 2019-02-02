08fb94f8659d1e246788de4b26ce6dc7b4174a96
Tyler Krtausch <krtauschboss@gmail.com>
2019-02-02 10:38:12 -0500

    CrazyRandomSword.cpp updated. Updated hit function to return 0 when damage is negetive

--- CrazyRandomSword.cpp
+++ CrazyRandomSword.cpp
@@ -7,5 +7,7 @@ double CrazyRandomSword::hit(double armor)
     int arm = armor/3;
     double damage = hitPoints - (armor - (rand() % arm + 2));
 
+    if(damge < 0)
+	return 0;
     return damage;
 }
