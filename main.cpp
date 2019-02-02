127fdb42395eb8e2458af820f7c4321f8029e68d
Tyler Krtausch <krtauschboss@gmail.com>
2019-02-02 11:00:00 -0500

    main.cpp updated. Added BattleAxe functionality

--- main.cpp
+++ main.cpp
@@ -51,6 +51,10 @@ int main(int argc, char** argv) {
     simulateWeapon(weapon, armor);
     delete (weapon);
 
+    weapon = WeaponFactory::getInstance()->getWeapon("Battle axe");
+    simulateWeapon(weapon, armor);
+    delete (weapon);
+
     return 0;
 }
 
